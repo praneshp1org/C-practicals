2adcee103278c57ba968d1b101728169381afcce
Pranesh Pyara Shrestha <info.praneshpyara@gmail.com>
2021-06-15 23:08:16 +0545

    Something added, I forgot

--- Assignment3/main.c
+++ Assignment3/main.c
@@ -3,7 +3,8 @@
 
 void main()
 {
-    negativeNumber();
+    //negativeNumber();
+    largestNumber();
 }
 void negativeNumber()
 {
@@ -14,5 +15,30 @@ void negativeNumber()
     if(num<0)
     {
         printf("NEGATIVE NUMBER!");
+    }
+}
+void largestNumber()
+{
+    int num1, num2, num3;
+    printf("Three numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    if(num1>num2)
+    {
+        if(num1>num3)
+        {
+            printf("%d is largest. ", num1);
+        }else
+        {
+            printf("%d is largest. ", num3);
+        }
+    }else
+    {
+        if(num2>num3)
+        {
+            printf("%d is largest.", num2);
+        }else
+        {
+            printf("%d is largest", num3);
+        }
     }
 }
